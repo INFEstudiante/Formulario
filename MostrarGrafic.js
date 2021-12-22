86c38bdea95ff54787f30aeb24e716b6564e053d
INFEstudiante <96504260+INFEstudiante@users.noreply.github.com>
2021-12-21 20:56:15 -0400

    Add files via upload

---
+++ MostrarGrafic.js
@@ -0,0 +1,26 @@
+document.querySelector(".addParam").addEventListener("click",addParam);
+document.querySelector(".showResults").addEventListener("click",showResults);
+
+var parametros = [];
+var valores = [];
+
+//funcion para agregar mas datos
+function addParam(){
+    let html = document.querySelector(".container").innerHTML;
+    let newHTML = '<div><input type="text" class="parametro" placeholder="parametro"><input type="number" class="valor" placeholder="valor"></div>'; 
+    document.querySelector(".container").innerHTML = html + newHTML;
+} 
+
+//funcion para mostrar el grafico
+function showResults(){
+    for (var i = 0; i < document.querySelectorAll('.parametro').length ; i++) {
+      parametros.push(document.querySelectorAll('.parametro')[i].value);
+      valores.push(parseInt(document.querySelectorAll(".valor")[i].value));
+    }
+    var data = [{
+      x: parametros,
+      y: valores,
+      type: "bar"
+    }];
+    Plotly.newPlot("grafico",data);
+}
